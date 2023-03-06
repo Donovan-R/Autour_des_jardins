7b184f8f2e3b6f075121db717a400990def2d85b
donovan <donoriviere@gmail.com>
2023-03-06 14:26:30 +0100

    add contactTitle coord

--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -4,12 +4,27 @@ import reglement from '.././assets/Jardins-reglement-interieur.pdf';
 import Form from '../components/Form';
 import { FaRegPaperPlane } from 'react-icons/fa';
 import { GiBirdHouse } from 'react-icons/gi';
+import { GoMail, GoHome } from 'react-icons/go';
+import { SiHomeassistant } from 'react-icons/si';
 
 const Contact = ({ alert, showAlert, setToken }) => {
   return (
     <section className='contactSection'>
       <div className='contactTitle'>
         <h2>Contact</h2>
+        <div className='underlineContact'></div>
+        <span>
+          <GoHome /> Mairie de Chéreng, 66 Route nationale 59152 Chéreng
+        </span>
+        <span>
+          <SiHomeassistant /> rue des tilleuls, Chéreng
+        </span>
+        <span className='mailPotager'>
+          <a href='mailto:potagers.autour.chereng@gmail.com'>
+            <GoMail /> potagers.autour.chereng@gmail.com
+          </a>
+        </span>
+
         <div className='underlineContact'></div>
         <h4>
           Prêt à cultiver votre jardin? Remplissez le formulaire ci-dessous pour
@@ -33,11 +48,12 @@ const Contact = ({ alert, showAlert, setToken }) => {
           <GiBirdHouse />
         </span>
       </Link>
-      <div className='goUp'>
+
+      <span className='goUp'>
         <a href='#'>
           <FaRegPaperPlane />
         </a>
-      </div>
+      </span>
     </section>
   );
 };

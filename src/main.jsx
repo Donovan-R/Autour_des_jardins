1a09dbcdd0369f97ae2aa31f498f35fd056ba160
donovan <donoriviere@gmail.com>
2023-03-06 15:24:09 +0100

    add break point contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Div100vh from 'react-div-100vh';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Div100vh>
-    <App />
-  </Div100vh>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
